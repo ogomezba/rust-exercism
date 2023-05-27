26abd23306e8122591eb13e2fdf597f7cdc32ba5
Oscar Gomez <oscargomezbalaguer@gmail.com>
2023-05-27 02:01:15 +0200

    anagram - second version
    Improved after realising that to_lowercase already creates a new String
    so there is no need to clone.
    In addition, the condition gets expressed as a group of && which makes
    it more clear

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,26 +1,17 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let lower_case_word = word.clone().to_lowercase();
-
-    let sorted_word = clone_and_sort_and_lower(word).to_lowercase();
+    let lower_case_word = word.to_lowercase();
+    let lower_case_sorted_word = lower_case_and_sort(word);
     let mut anagrams = HashSet::new();
 
     possible_anagrams
         .iter()
-        .filter(|&candidate| {
-            if word.len() != candidate.len() {
-                false
-            } else {
-                let lower_case_candidate = candidate.clone().to_lowercase();
-                let sorted_candidate = clone_and_sort_and_lower(candidate);
-
-                if lower_case_candidate == lower_case_word {
-                    false
-                } else {
-                    sorted_candidate == sorted_word
-                }
-            }
+        .filter(|&&candidate| {
+            let lower_case_candidate = candidate.to_lowercase();
+            candidate.len() == word.len()
+                && lower_case_candidate != lower_case_word
+                && lower_case_sorted_word == lower_case_and_sort(candidate)
         })
         .for_each(|&candidate| {
             anagrams.insert(candidate);
@@ -29,15 +20,8 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'
     anagrams
 }
 
-pub fn clone_and_sort_and_lower(word: &str) -> String {
-    let char_array: Vec<char> = word.chars().collect();
-
-    let mut string_array: Vec<String> = char_array
-        .iter()
-        .map(|char| char.to_lowercase().to_string())
-        .collect();
-
-    string_array.sort_unstable();
-
-    string_array.join("")
+fn lower_case_and_sort(word: &str) -> String {
+    let mut chars: Vec<char> = word.to_lowercase().chars().collect();
+    chars.sort_unstable();
+    chars.into_iter().collect()
 }
