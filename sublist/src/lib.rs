ef53917a540121a9775a6ae55ab2a3ba36a1edcf
Oscar Gomez <oscargomezbalaguer@gmail.com>
2023-05-29 15:47:36 +0200

    sublist - second version
    Checked the solution from [alireza4050](https://exercism.org/tracks/rust/exercises/sublist/solutions/alireza4050)
    on exercism an discovered about the windows() function. Implemented it
    using a far more functional approach and the windows function (it
    resulted in the same solution as there is not a lot to deviate )

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,5 +1,3 @@
-use std::fmt::Display;
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum Comparison {
     Equal,
@@ -8,67 +6,31 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq + Display>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    if _first_list.is_empty() && _second_list.is_empty() {
-        return Comparison::Equal;
-    }
-    if _first_list.is_empty() && !_second_list.is_empty() {
-        return Comparison::Sublist;
-    }
-    if !_first_list.is_empty() && _second_list.is_empty() {
-        return Comparison::Superlist;
-    }
-    if _first_list == _second_list {
-        return Comparison::Equal;
-    }
-
-    let first: &[T];
-    let second: &[T];
-    let mut reversed = false;
-
-    if _first_list.len() >= _second_list.len() {
-        first = _first_list;
-        second = _second_list;
-    } else {
-        first = _second_list;
-        second = _first_list;
-        reversed = true;
-    }
-
-    let mut fp = 0;
-    let mut fp2 = 0;
-    let mut sp = 0;
-    let mut fe;
-    let mut se;
-
-    while fp < first.len() {
-        fe = first.get(fp2).unwrap();
-        se = second.get(sp).unwrap();
-
-        while fe == se && sp < second.len() {
-            if sp == second.len() - 1 {
-                if reversed {
-                    return Comparison::Sublist;
-                } else {
-                    return Comparison::Superlist;
-                }
+pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    match (_first_list.len(), _second_list.len()) {
+        (0, 0) => Comparison::Equal,
+        (_, 0) => Comparison::Superlist,
+        (0, _) => Comparison::Sublist,
+        (m, n) if m > n => {
+            if _first_list.windows(n).any(|w| w == _second_list) {
+                Comparison::Superlist
+            } else {
+                Comparison::Unequal
             }
-
-            fp2 = fp2 + 1;
-            sp = sp + 1;
-
-            if fp2 == first.len() || sp == second.len() {
-                break;
-            }
-
-            fe = first.get(fp2).unwrap();
-            se = second.get(sp).unwrap();
         }
-
-        sp = 0;
-        fp = fp + 1;
-        fp2 = fp;
+        (m, n) if n > m => {
+            if _second_list.windows(m).any(|w| w == _first_list) {
+                Comparison::Sublist
+            } else {
+                Comparison::Unequal
+            }
+        }
+        (_, _) => {
+            if _first_list == _second_list {
+                Comparison::Equal
+            } else {
+                Comparison::Unequal
+            }
+        }
     }
-
-    Comparison::Unequal
 }
